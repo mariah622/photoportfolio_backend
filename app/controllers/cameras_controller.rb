9712a837128b1b2d9b427f6573bee805893e4e38
mariah622 <mjustine622@gmail.com>
2022-03-08 18:55:27 -0500

    create & delete completed

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -1,7 +1,38 @@
 class CamerasController < ApplicationController
-    def index 
-        cameras = Camera.all
-        render json: cameras
-        
-    end 
+    def index
+      @camera = Camera.all
+  
+      render json: @camera
+    end
+  
+    def show 
+        @camera = Camera.find(params[:id])
+        render json: @camera
+      end
+    
+      def create 
+        @camera = Camera.new(camera_params)
+    
+        if @camera.save 
+          render json: @camera 
+        else   
+          render json: @camera.errors, status: unprocessable_entity # {error: 'Error creating'}
+        end
+      
+      end
+
+      def destroy
+        @camera = Camera.find(params[:id])
+        @camera.destroy
+      end
+  
+
+  
+    private
+    
+    
+    def camera_params
+        params.permit(:brand, :name, :id, :camera_type, :overview)
+    end
 end
+
